7cf117b0e703379f75f6444393f62be88f34b764
ParkJinyang-hello <akemfpaldk987@gmail.com>
2025-06-11 15:48:55 +0900

    flask 설정2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,render_template, request
 
 # 객체생성
 app = Flask(__name__)
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', success=True)
+    return render_template('contact.html', success=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
