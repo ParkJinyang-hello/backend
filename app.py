2273b4574a21024a880263535a94a570ff91e5a8
ParkJinyang-hello <akemfpaldk987@gmail.com>
2025-06-11 16:29:47 +0900

    flask 4

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template, request, redirect, url_for, session
 
 # 객체생성
 app = Flask(__name__)
+app.secret_key = 'secret key'
+
 
 @app.route('/')
 def index():
@@ -19,10 +21,14 @@ def projects():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        print(f'제출된 데이터 이름:{name} 이메일:{email} 메세지:{message}')
         # 리다이렉트 success 표시
         session['success'] = True
-        return redirect(url_for('contact'))
+        return redirect(url_for('contact'))  # get 방식
+    
     # get요청시 success 표시 후 초기화
     success = session.pop('success', False)
     return render_template('contact.html', success=success)
